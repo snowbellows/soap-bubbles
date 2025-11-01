9b0d2abbfd2f63962f2bbb9f6e9ce9dd5e4cb7a0
Alex Halverson <alexhalverson@posteo.net>
2025-11-01 17:24:41 +1100

    bubbles bounce

--- sketch.js
+++ sketch.js
@@ -3,27 +3,37 @@ const maxSoapBubbles = 500;
 let soapBubbles = [];
 let newBubbleJet = false;
 let bubbleColour = '#6DAEDB';
+let colours = [
+  '#6DAEDB',
+  '#EF6461',
+  '#E4B363',
+  '#4CB944',
+  '#29335C',
+  '#861657',
+];
 
 function setup() {
   createCanvas(800, 600);
   // make it explicit that we're using ellipse centre mode
-  // ellipseMode(CENTER);
+  ellipseMode(CENTER);
+
+  soapBubbles = generateBubbles();
 }
 
 function draw() {
   background(220);
 
-  soapBubbles = constrainSoapBubbles(soapBubbles);
+  // soapBubbles = constrainNumBubbles(soapBubbles);
   soapBubbles = soapBubbles.map(updateSoapBubble);
   soapBubbles.forEach(drawSoapBubble);
-  newBubbleJet = checkForJet();
-  if (newBubbleJet) {
-    soapBubbles = addNewBubble(soapBubbles);
-  }
+  // newBubbleJet = checkForJet();
+  // if (newBubbleJet) {
+  //   soapBubbles = addNewBubble(soapBubbles);
+  // }
 }
 
 function checkForJet() {
-  // jet on even seconds
+  // jet every 5 seconds
   if (second() % 5 === 0) {
     return true;
   } else {
@@ -33,9 +43,25 @@ function checkForJet() {
 
 // BUBBLES
 
-function constrainSoapBubbles(soapBubbles) {
+function generateBubbles() {
+  let bubbles = [];
+  for (let i = 0; i < 6; i++) {
+    const x = (width / 6) * i + width / 6 / 2;
+
+    for (let j = 0; j < 6; j++) {
+      const y = (height / 6) * j + height / 6 / 2;
+      bubbles.push(createNewBubble([x, y]));
+    }
+  }
+  return bubbles;
+}
+
+function constrainNumBubbles(soapBubbles) {
   if (soapBubbles.length > maxSoapBubbles) {
-    return soapBubbles.slice(0, soapBubbles.length - 1);
+    return soapBubbles.slice(
+      soapBubbles.length - maxSoapBubbles,
+      soapBubbles.length
+    );
   }
   return soapBubbles;
 }
@@ -80,15 +106,31 @@ function drawSoapBubble(b) {
 }
 
 function updateSoapBubble(b) {
-  const velocity = { x: b.velocity.x, y: b.velocity.y + 0.0001 };
+  const gb = simulateGravity(b);
+  const bb = checkbounds(gb);
+  return bb;
+}
+
+function simulateGravity(b) {
+  const velocity = { x: b.velocity.x, y: b.velocity.y + 9.8 / 100 };
   const position = {
     x: b.position.x,
-    y: b.position.y + b.position.y * velocity.y,
+    y: b.position.y + velocity.y,
   };
 
-  if (position.y > height) {
+  return {
+    ...b,
+    position,
+    velocity,
+  };
+}
+
+function checkbounds(b) {
+  let position = b.position;
+  let velocity = b.velocity;
+  if (b.position.y > height) {
     position.y = height;
-    velocity.y = -0.01;
+    velocity.y = -b.velocity.y;
   }
 
   return {
